5e959f83697f5ccae4fcfdc66ed1b3b26566296d
AbhishekSingh-glitch <148774410+AbhishekSingh-glitch@users.noreply.github.com>
2023-11-01 18:52:37 +0530

    Add files via upload

---
+++ sumof2no.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main ()
+{
+    int a,b,c;
+    printf("enter no. a");
+    scanf("%d",&a);
+    printf("enter no. b");
+    scanf("%d",&b);
+    c=a+b;
+    printf("result %d",c);
+    return 0;
+    }
