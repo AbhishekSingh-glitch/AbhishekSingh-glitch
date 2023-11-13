84b57d21c392112dd0ac2b8865c1ae0867728f52
AbhishekSingh-glitch <148774410+AbhishekSingh-glitch@users.noreply.github.com>
2023-11-13 10:30:32 +0530

    add_of_sum_minus.c

---
+++ add_of_sum_minus.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    scanf("%d%d",&a,&b);
+    int sum=a+b;  //take any natural no. or
+    int sub=a-b;  //if you want decimal change int with float
+    int add =sum-sub;
+    printf("%d",add);
+    return 0;
+}
