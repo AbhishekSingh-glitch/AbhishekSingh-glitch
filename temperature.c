85e63864b8f0802a18d1da4c7d96d27d433a04f8
AbhishekSingh-glitch <148774410+AbhishekSingh-glitch@users.noreply.github.com>
2023-11-14 10:39:37 +0530

    temperature.c

---
+++ temperature.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+int main()
+
+celsius to fahrenheit
+/*{
+    float c;
+    scanf("%f",&c); //c is celsius
+    float f = c*((float)9/5) + 32;
+    printf("%f",f);  //f is fahrenheit
+    return 0;
+}*/
+
+fahrenheit to celsius
+/*{
+    float f;
+    scanf("%f",&f);
+    float c = f*((float)9/5) + 32;
+    printf("%f",c);
+    return 0;
+}*/
+
+//kelvin to fahrenheit
+/*{
+    float k;  //k is kelvin
+    scanf("%f",&k);
+    float f = (k-273.15)* 9/5 + 32;
+    printf("%f",f);
+    return 0;  
+}*/
+
+//fahrenheit to kelvin 
+/*{
+    float f;
+    scanf("%f",&f);
+    float k =(f-32)*5/9 + 273.15 ;
+    printf("%f",k);
+    return 0;  
+}*/
+
+//celeius to kelvin
+/*{
+    float c;
+    scanf("%f",&c);
+    float k = c + 273.15;
+    printf("%f",k);
+    return 0;  
+}*/
+
+//kelvin to celsius
+/*{
+    float k;
+    scanf("%f",&k);
+    float c = k - 273.15;
+    printf("%f",c);
+    return 0;    
+}*/
