a307e5ef39948cdbe5308c399fcb473d5f351f6d
AbhishekSingh-glitch <148774410+AbhishekSingh-glitch@users.noreply.github.com>
2023-11-15 18:17:41 +0530

    nameage.c

--- nameage.c
+++ nameage.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 int main()
 {
-char name;
+char name;     //for more char[number]
 int age ;
 printf("name");
-scanf("%c",& name); //print only 1 char
+scanf("%c",& name); //print only 1 char 
+//chars %s
 printf("age");
 scanf("%d",& age);
 return 0;
