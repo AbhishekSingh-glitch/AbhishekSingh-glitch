b90dbc05729d012287d352dafc039bca531c2997
Abhishek Singh <148774410+AbhishekSingh-glitch@users.noreply.github.com>
2023-11-24 13:49:52 +0530

    ASCIIcode.c

---
+++ ASCIIcode.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+inr main ()
+
+//ASCII to Character
+/*{
+    int ascii;
+    printf("Enter ASCII code: ");
+    scanf("%d",&ascii);
+    printf("%d(ASCII) = %c(corresponding character)",ascii,ascii);
+    return 0;
+}*/
+
+//Character to ASCII
+/*{
+    int n1;
+    char ascii;
+    printf("Enter a character: ");
+    scanf("%c",&ascii);
+    printf("%c(character) = %d(corresponding ASCII code)",ascii,ascii);
+    return 0;
+}*/
