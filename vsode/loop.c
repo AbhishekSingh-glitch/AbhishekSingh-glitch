1f63922e70022d5817e35568f085680421d40a3c
Abhishek Singh <148774410+AbhishekSingh-glitch@users.noreply.github.com>
2024-03-08 19:57:13 +0530

    Add files via upload

---
+++ vsode/loop.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main ()
+/*{
+    char j,n;
+    printf("enter char");
+    scanf("%c",&n);
+    for (j=n;j,j<=n+1;j=j+n)
+    {
+        printf("%d\n",j);
+    }
+    return 0;
+}*/
