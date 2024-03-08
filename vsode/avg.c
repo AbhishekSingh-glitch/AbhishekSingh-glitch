c7a0d1d2fbc4dc2947456ce3e1158aa67ed7fdfc
Abhishek Singh <148774410+AbhishekSingh-glitch@users.noreply.github.com>
2024-03-08 19:55:50 +0530

    Add files via upload

---
+++ vsode/avg.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main ()
+/*{
+    int w,x,y;
+    scanf("%d%d%d",&w,&x,&y);
+    float z = (float)(w + x + y)/3;
+    printf("%f",z);
+    return 0;
+}*/
+// done
+/*{
+    int n,count;
+    float sum =0,x,avg;
+    printf("enter how many number\n");
+    scanf("%d",&n);
+    for (count=1; count<=n; count++)
+    {
+        printf("x= ");
+        scanf("%f",&x);
+        sum += x;
+    }
+    avg = sum/n;
+    printf("\naverage %f",avg);
+    
+    return 0;
+}*/
