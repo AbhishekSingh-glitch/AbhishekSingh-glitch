e5e2830cbb663864e72dee760631d2160763e2c4
Abhishek Singh <148774410+AbhishekSingh-glitch@users.noreply.github.com>
2024-03-08 19:49:41 +0530

    Add files via upload

---
+++ 9questions.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+int main() 
+
+/*{
+    float a,b,c,d,e;
+    scanf("%f%f%f%f%f",&a,&b,&c,&d,&e );
+    float f=a*b+c/d-e;
+    printf("%f",f);   //1
+    return 0;
+}*/
+
+
+/*{
+    int a,b,c,d;
+    scanf("%d%d%d%d", &a,&b ,&c,&d);
+    float result=(a+b)*c%d;
+    printf("%f", result);    //2
+    return 0;
+}*/
+
+/*{
+    int a;
+    a=5&&3||0;
+    printf("%d",a);   //3
+    return 0;
+}*/
+
+/*{
+    int x,y;
+    (x=y=2+3);
+    printf("%d\n%d",x,y);   //4
+    return 0;
+}*/
+
+/*{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    a>10 && b<5?printf("1"):printf("0");   //5
+    return 0;
+}*/
+
+/*{
+  float a, b, c, d, e;
+  scanf("%f%f%f%f%f",&a,&b,&c,&d,&e);
+  float result = (a+b*c-d/e);   //6
+  printf("%f", result);
+  return 0;
+}*/
+
+/*{
+    int x=5,y;
+    y=x++ + ++x;       //7
+    printf("%d",y); 
+    return 0;
+
+}*/
+
+/*{
+  float a,b,c,d,e,f;
+  scanf("%f%f%f%f%f",&a,&b,&c,&d,&e);
+  f = a*(b+c)%(d-e);   //8
+  printf("%f",f);
+  return 0;
+}*/
+
+
+/*{
+    int a;
+    printf("write a random number");
+    scanf("%d",&a);
+    a<0?printf("1"): printf("0");    //10
+    return 0;
+}*/
