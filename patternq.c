010d693a86d2a1abd6bbd3a227bd449718e16f38
Abhishek Singh <abhishek120106@gmail.com>
2024-04-11 23:53:47 +0530

    Update patternq.c

--- patternq.c
+++ patternq.c
@@ -2,27 +2,68 @@
 // * * * *
 // * * * *
 // * * * *
+// #include<stdio.h>
+// int main(){
+//     for(int i=0;i<4;i++){
+//         for(int j=0;j<4;j++)
+//         printf("* ");
+//         printf("\n");
+//     }
+// }
 
 // 1 1 1 1 
 // 2 2 2 2 
 // 3 3 3 3  
 // 4 4 4 4
+// #include<stdio.h>
+// int main(){
+//     for(int i=1;i<=4;i++){
+//         for(int j=0;j<4;j++)
+//         printf("%d ",i);
+//         printf("\n");
+//     }
+// }
 
 //  1 2 3 4 
 //  1 2 3 
 //  1 2 
-//  1  
+//  1
+// #include<stdio.h>
+// int main(){
+//     for(int i=1;i<=4;i++){
+//         for(int j=1;j<=4;j++)
+//         if(i+j<=5)
+//         printf("%d ",j);
+//         printf("\n");
+//     }
+// }
 
 //  A 
 //  A B 
 //  A B C 
 //  A B C D
+// #include<stdio.h>
+// int main(){
+//     for(int i=65;i<69;i++){
+//         for(char j='A';j<=i;j++)
+//         printf("%c ",j);
+//         printf("\n");
+//     }
+// }
 
 //  1 
 //  A B 
 //  1 2 3 
 //  A B C D 
-//  1 2 3 4 5  
+//  1 2 3 4 5
+#include<stdio.h>
+int main(){
+    for(int i=1;i<=4;i++){
+        for(int j=1;j<=4;j++)
+        
+        printf("\n");
+    }
+}
 
 // * 
 // * * 
@@ -46,6 +87,14 @@
 // AB
 // ABC
 // ABCD
+// #include<stdio.h>
+// int main(){
+//     for(int i=65;i<69;i++){
+//         for(char j='A';j<=i;j++)
+//         printf("%c",j);
+//         printf("\n");
+//     }
+// }
 
 // 1
 // 2 1
