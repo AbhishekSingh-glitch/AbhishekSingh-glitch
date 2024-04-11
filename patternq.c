5fe005b3be0e31d0be035220e2757f0d741d3d95
Abhishek Singh <abhishek120106@gmail.com>
2024-04-11 23:33:30 +0530

    Create patternq.c

---
+++ patternq.c
@@ -0,0 +1,125 @@
+// * * * * 
+// * * * *
+// * * * *
+// * * * *
+
+// 1 1 1 1 
+// 2 2 2 2 
+// 3 3 3 3  
+// 4 4 4 4
+
+//  1 2 3 4 
+//  1 2 3 
+//  1 2 
+//  1  
+
+//  A 
+//  A B 
+//  A B C 
+//  A B C D
+
+//  1 
+//  A B 
+//  1 2 3 
+//  A B C D 
+//  1 2 3 4 5  
+
+// * 
+// * * 
+// * * * 
+// * * * * 
+// * * * 
+// * *  
+// * 
+
+// * * * *
+// *     *
+// *     *
+// * * * *
+
+//  1 
+//  1 2 
+//  1 2 3 
+//  1 2 3 4  
+
+// A
+// AB
+// ABC
+// ABCD
+
+// 1
+// 2 1
+// 3 2 1
+// 4 3 2 1
+
+// Write a C program to print the following Pattern.
+//    1
+//   123
+//  12345
+// 1234567
+
+// Write a C program to print the following Pattern.
+//    A 
+//   ABC
+//  ABCDE
+// ABCDEFG
+
+// Write a C program to print the following Pattern.
+//    A 
+//   BAB
+//  CBABC
+// DCBABCD
+
+// Write a C program to print the following Pattern.
+//    1
+//   2 2
+//  3   3
+// 4     4
+
+// Write a C program to print the following Pattern.
+// *     *
+//  *   *
+//   * *
+//    *
+
+// 1234321
+// 123 321
+// 12   21
+// 1     1
+
+// Write a C program to print the following Pattern.
+// ABCDEFG
+// ABC EFG
+// AB   FG
+// A     G
+
+// Write a C program to print the following Pattern.
+//    *
+//   * *
+//  *   *
+// *     *
+//  *   *
+//   * *
+//    *
+
+// Write a C program to print the following Pattern.
+// *******
+// *** ***
+// **   **
+// *     *
+// **   **
+// *** ***
+// *******
+
+// Write a C program to print the following Pattern.
+//       *
+//     * * *
+//    *  *  *
+//   *   *   *
+//  *    *    *
+// ***********
+//  *    *    *
+//   *   *   *
+//    *  *  *
+//     * * *
+//       *
