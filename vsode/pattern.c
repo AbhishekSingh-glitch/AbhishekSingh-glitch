931c1d68856e06670b9529360e4e8f91fc9a7cc6
Abhishek Singh <abhishek120106@gmail.com>
2024-04-12 08:59:45 +0530

    Update pattern.c

--- vsode/pattern.c
+++ vsode/pattern.c
@@ -202,24 +202,25 @@
 //     }                  
 // }
 
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main() {
-//     int n;
-//     scanf("%d",&n);
-//     int k=0;
-//     for(int i=1;i<=n;++i){
-//         if(i<=n/2+1) ++k;
-//         else --k;
+int main() {
+    int n;
+    printf("odd no. ");
+    scanf("%d",&n);
+    int k=0;
+    for(int i=1;i<=n;++i){
+        if(i<=n/2+1) ++k;
+        else --k;
     
-//         for(int j=1;j<=n;++j){
-//             if(j>=n/2+k+1 || j<=n/2-k+1) printf("  ");
-//             else printf("* ");
-//         }
-//         printf("\n");
-//     }
-//     return 0;
-// }
+        for(int j=1;j<=n;++j){
+            if(j>=n/2+k+1 || j<=n/2-k+1) printf("  ");
+            else printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
 
 // #include <stdio.h>
 
@@ -240,69 +241,69 @@
 //       return 0;
 // }
 
-#include<stdio.h>
-int main ()
-{
-    int a=0,i=0;
-    char s[10];
-    gets(s);
-    while(s[i]!='\n'){
-        if(s[i]=='a')
-        ++a;
-    ++i;
-    }
-    puts(s);
-    printf("\n%d times 'a' is used",a);
-    return 0;
+// #include<stdio.h>
+// int main ()
+// {
+//     int a=0,i=0;
+//     char s[10];
+//     gets(s);
+//     while(s[i]!='\n'){
+//         if(s[i]=='a')
+//         ++a;
+//     ++i;
+//     }
+//     puts(s);
+//     printf("\n%d times 'a' is used",a);
+//     return 0;
 
-}
-
-
-#include<stdio.h>
-int main ()
-{
-    int a=0,i=0;
-    char s[100];
-    gets(s);
-    while(s[i]!='\n'){
-        if(s[i]==' ')
-        a++;
-    ++i;
-    }
-    puts(s);
-    printf("\n%d times 'space' is used",a);
-    return 0;
-}
+// }
 
 
-#include<stdio.h>
-int main ()
-{
-    int a=0,i=0;
-    char s[10];
-    gets(s);
-    while(s[i]!='\n'){
-        if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'
-         ||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U')
-        a++;
-    ++i;
-    }
-    puts(s);
-    printf("\n%d times 'vowel' is used",a);
-return 0;
-}
+// #include<stdio.h>
+// int main ()
+// {
+//     int a=0,i=0;
+//     char s[100];
+//     gets(s);
+//     while(s[i]!='\n'){
+//         if(s[i]==' ')
+//         a++;
+//     ++i;
+//     }
+//     puts(s);
+//     printf("\n%d times 'space' is used",a);
+//     return 0;
+// }
 
 
-#include<stdio.h>
-int main ()
-{
-    int a=0,i=0;
-    char s[10];
-    for(char i='\n';i!=s[a-1];){
-        scanf("%c",&s[a]);++a;
-    }
+// #include<stdio.h>
+// int main ()
+// {
+//     int a=0,i=0;
+//     char s[10];
+//     gets(s);
+//     while(s[i]!='\n'){
+//         if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'
+//          ||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U')
+//         a++;
+//     ++i;
+//     }
+//     puts(s);
+//     printf("\n%d times 'vowel' is used",a);
+// return 0;
+// }
+
+
+// #include<stdio.h>
+// int main ()
+// {
+//     int a=0,i=0;
+//     char s[10];
+//     for(char i='\n';i!=s[a-1];){
+//         scanf("%c",&s[a]);++a;
+//     }
     
-    puts(s);
-    return 0;
-}
+//     puts(s);
+//     return 0;
+// }
 
