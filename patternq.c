a85a037411a9a7ff0b92e12e16421e4ef361d45f
Abhishek Singh <148774410+AbhishekSingh-glitch@users.noreply.github.com>
2024-04-14 23:24:59 +0530

    Update patternq.c

--- patternq.c
+++ patternq.c
@@ -110,6 +110,18 @@
 // *     *
 // *     *
 // * * * *
+#include <stdio.h>
+int main() {
+  int a;
+  scanf("%d", &a);
+  for (int i = 1; i <= a; i++) {
+    for (int j = 1; j <= a; j++)
+      if (i == a - (a - 1) || i == a || j == a - (a - 1) || j == a)
+        printf("* ");
+        else printf("  ");
+    printf("\n");
+  }
+}
 
 //  1            //done
 //  1 2 
