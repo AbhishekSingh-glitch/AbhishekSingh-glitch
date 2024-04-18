5249daf7d532cac8bec7674918345187071881ba
Abhishek Singh <abhishek120106@gmail.com>
2024-04-18 21:40:22 +0530

    Create greatest.c

---
+++ sems2/Array/greatest.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a[10],l=0,i=0;
+
+    for(i=0;i<10;i++)
+    scanf("%d",&a[i]);
+    
+    for(i=0;i<10;i++)
+    if(l<a[i])
+    l=a[i];
+
+    printf("%d ",l);
+       
+}
