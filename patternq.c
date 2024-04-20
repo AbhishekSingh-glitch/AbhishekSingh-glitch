59fad39b2c77227320d27ceaa6c48db5b20409b2
Abhishek Singh <abhishek120106@gmail.com>
2024-04-20 22:18:53 +0530

    Update patternq.c

--- patternq.c
+++ patternq.c
@@ -437,52 +437,62 @@
 //   *  *  *
 //    * * *
 //      *
-
 #include <stdio.h>
 int main() {
+  printf("Enter a no.");
   int a;
   scanf("%d", &a);
-  for (int i = 0; i < (a +1)/2; i++) {
+  for (int i = 0; i < a ; i++) {
 
-    for (int j = (a +1)/2; j > i + 1; j--)
+    for (int j = a ; j > i + 1; j--)
       printf(" ");
-    for (int j = 0; j < 1; j++)
-      printf("*");
-    for (int j = 2; j <= i; j++)
-      printf(" ");
-    for (int j = 0; j < 1; j++)
+      for (int j = 0; j < 1; j++)
+        printf("*");
+      
+      for (int j = 2; j <= i; j++)
+        printf(" ");
+      
+      for (int j = 0; j < 1; j++)
       if (i > 0)
         printf("*");
-    for (int j = 2; j <= i; j++)
+      
+      for (int j = 2; j <= i; j++)
       printf(" ");
-    for (int j = 0; j < 1; j++)
+      
+      for (int j = 0; j < 1; j++)
       if (i > 0)
         printf("*");
 
-    printf("\n");
+      printf("\n");
   }
+  
+  for (int h = 0; h < a+(a-1); h++)
+    printf("*");  
+    
+    printf("\n");
 
-  for (int h = 0; h < a; h++)
-    printf("*");
-
-  printf("\n");
-  for (int i = 0; i < (a +1)/2 ; i++) {
+  for (int i = 0; i < a  ; i++) {
 
     for (int j = 1; j <= i; j++)
-      printf(" ");
+      printf(" "); 
+    
     for (int j = 0; j<1; j++)
       printf("*");
-    for (int j = (a +1)/2 ; j > i + 2; j--)
+      
+    for (int j = a ; j > i+2 ; j--)
       printf(" ");
+      
     for (int l = 0; l < 1; l++)
-    if(i<((a +1)/2)-1)
-      printf("*");
-    for (int j = (a +1)/2 ; j > i + 2; j--)
-      printf(" ");
-    for (int j = 0; j < 1; j++)
-    if(i<(a +1)/2-1)
-      printf("*");
-
+      if(i<a -1)
+        printf("*");
+      
+    for (int j = a ; j > i+2 ; j--)
+      printf(" ");  
+      
+    for (int j = 0; j<1; j++)
+    if(i<a -1)
+      printf("*");  
+      
     printf("\n");
-  }
+    }
 }
