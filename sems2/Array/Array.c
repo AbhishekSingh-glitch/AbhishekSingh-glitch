a774edf1de4ef1a116f38d6878308e6d33099b31
Abhishek Singh <abhishek120106@gmail.com>
2024-04-21 22:57:37 +0530

    Update Array.c

--- sems2/Array/Array.c
+++ sems2/Array/Array.c
@@ -73,20 +73,41 @@ int main(){
 //    printf("%d",a[0]);
 // }
 
+// #include<stdio.h>
+// int main(){
+//     int a[5],x,f=0;
+    
+//     for(int i=1;i<=5;i++)
+//         scanf("%d",&a[i]);
+
+//     printf("Enter element");
+//     scanf("%d",&x);
+
+//     for(int i=1;i<=5;i++)
+//         if(a[i]==x){
+//     printf("%d ",i);f=1;}
+
+//     if(f==0) printf("-1");
+
+// }
+
+// #include<stdio.h>
+// int main(){
+//     int a[5],b,cnt=0;
+    
+//     for(int i=0;i<5;i++)
+//         scanf("%d",&a[i]);
+
+//     scanf("%d",&b);
+    
+//     for(int i=0;i<5;i++)
+//         if(a[i]>b)
+//             cnt++;
+    
+//     printf("%d",cnt);
+// }
+
 #include<stdio.h>
 int main(){
-    int a[5],x,f=0;
     
-    for(int i=1;i<=5;i++)
-        scanf("%d",&a[i]);
-
-    printf("Enter element");
-    scanf("%d",&x);
-
-    for(int i=1;i<=5;i++)
-        if(a[i]==x){
-    printf("%d ",i);f=1;}
-
-    if(f==0) printf("-1");
-
 }
