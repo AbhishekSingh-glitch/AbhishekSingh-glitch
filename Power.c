5b1a6066e522e8a33b59ca486240600543fa1fa5
Abhishek Singh <abhishek120106@gmail.com>
2024-04-23 20:44:18 +0530

    Update Power.c

--- Power.c
+++ Power.c
@@ -1,64 +1,47 @@
 #include<stdio.h>
-int main()
-{
-int a;
-int b;
-int d=1;
+int main() {
+    int a,b,d=1;
+    scanf("%d%d",&a,&b);
 
-scanf("%d%d",&a,&b);
+    for(int c;c<b; ++c)
+    d*=a;
 
-for(int c;c<b; ++c){
-d*=a;
-}
-printf("%d",d);
-return 0;
+    printf("%d",d);
+    return 0;
 }
 
 #include<stdio.h>
-int main()
-{
-int a;
-int b;
-int d=1;
-
-scanf("%d%d",&a,&b);
-
-while(b!=0){
-d*=a;
---b;
-}
-printf("%d",d);
-return 0;
+int main(){
+    int a,b,d=1;
+    scanf("%d%d",&a,&b);
+    while(b!=0){
+        d*=a;
+        --b;
+    }
+    printf("%d",d);
+    return 0;
 }
 
 #include<stdio.h>
-int main()
-{
-int a;
-int b;
-int c=0;
-int d=1;
-
-scanf("%d%d",&a,&b);
-while(c<b){
-d*=a;
-++c;
-}
-printf("%d",d);
-return 0;
+int main(){
+    int a,b,c=0,d=1;
+    scanf("%d%d",&a,&b);
+    while(c<b){
+        d*=a;
+        ++c;
+    }
+    printf("%d",d);
+    return 0;
 }
 
 #include<stdio.h>
-int main()
-{
-int a, b,c=1, d=1;
-
-scanf("%d%d",&a,&b);
-while(c<=b){
-d*=a;
-++c;
-
-}
-printf("%d",d);
-return 0;
+int main(){
+    int a, b,c=1, d=1;
+    scanf("%d%d",&a,&b);
+    while(c<=b){
+        d*=a;
+        ++c;
+    }
+    printf("%d",d);
+    return 0;
 }
