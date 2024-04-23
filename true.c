d90f6bcad1d771b18853cb99f347727608a8ce04
Abhishek Singh <abhishek120106@gmail.com>
2024-04-23 09:16:19 +0530

    Update true.c

--- true.c
+++ true.c
@@ -1,49 +1,57 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main() {
-  int a;
-  scanf("%d", &a);
-  for (int i = 0; i < (a +1)/2; i++) {
+int main (){
+  int a; scanf("%d",&a);
+  for(int i=0;i<a;i++){
+    for(int j=a;j>i+1;j--)           
+      printf("*");         
 
-    for (int j = (a +1)/2; j > i + 1; j--)
-      printf(" ");
-    for (int j = 0; j < 1; j++)
-      printf("*");
-    for (int j = 2; j <= i; j++)
-      printf(" ");
-    for (int j = 0; j < 1; j++)
-      if (i > 0)
-        printf("*");
-    for (int j = 2; j <= i; j++)
-      printf(" ");
-    for (int j = 0; j < 1; j++)
-      if (i > 0)
-        printf("*");
+    for(int j=1;j<i+i;j++)       
+      printf(".");  
 
-    printf("\n");
-  }
+    for(int j=a;j>i+1;j--)
+      printf("*"); 
+                    
+      printf("\n");             
+  }            
+  for(int i=0;i<=a;i++){       
+    for(int j=0;j<i+1;j++)           
+      printf("^");           
+        
+    for(int j=a+i;j>=i;j--) 
+      printf("-");
 
-  for (int h = 0; h < a; h++)
-    printf("*");
-
-  printf("\n");
-  for (int i = 0; i < (a +1)/2 ; i++) {
-
-    for (int j = 1; j <= i; j++)
-      printf(" ");
-    for (int j = 0; j<1; j++)
-      printf("*");
-    for (int j = (a +1)/2 ; j > i + 2; j--)
-      printf(" ");
-    for (int l = 0; l < 1; l++)
-    if(i<((a +1)/2)-1)
-      printf("*");
-    for (int j = (a +1)/2 ; j > i + 2; j--)
-      printf(" ");
-    for (int j = 0; j < 1; j++)
-    if(i<(a +1)/2-1)
+    for(int j=0;j<i;j++)
       printf("*");
 
     printf("\n");
-  }
+  }                  
 }
+
+
+
+
+
+
+
+// #include<stdio.h>
+
+// int main (){
+//     int a=4;                
+//     for(int i=0;i<a;i++){
+//         for(int j=0;j<i;j++)      //  * * * * *
+//         printf(" ");              //   * * * * 
+//         for(int j=a;j>=i;j--){    //    * * * 
+//             printf("# ");         //     * * 
+//         }                         //      * 
+//         printf("\n");             //     * *
+//     }                             //    * * *  
+//     for(int i=0;i<=a;i++){        //   * * * *
+//         for(int j=a;j>i;j--)      //  * * * * *
+//         printf(" ");              
+//         for(int j=0;j<=i;j++){    
+//             printf("* ");         
+//         }                         
+//         printf("\n");
+//     }
+// }
