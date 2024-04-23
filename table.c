c2e9ac5097685552e47a6344a878f8278252117f
Abhishek Singh <abhishek120106@gmail.com>
2024-04-23 20:54:41 +0530

    Update table.c

--- table.c
+++ table.c
@@ -1,13 +1,13 @@
-// #include<stdio.h>
-// int main ()
-// {
-//     int n;
-//     int i;
-//     printf("Which table you want\n");
-//     scanf("%d",&n);
-//     for(i=n;i<=n*10;i=i+n)  //increase the zeroes you want
-//     {
-//         printf("%d\n",i);
-//     }
-//     return 0;
-// }
+#include<stdio.h>
+int main ()
+{
+    int n;
+    int i;
+    printf("Which table you want\n");
+    scanf("%d",&n);
+    for(i=n;i<=n*10;i=i+n)  //increase the zeroes you want
+    {
+        printf("%d\n",i);
+    }
+    return 0;
+}
