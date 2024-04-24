c3cccc6079147e5a349f13cb3a9f0f035d77d712
Abhishek Singh <abhishek120106@gmail.com>
2024-04-24 18:36:23 +0530

    Update pattern.c

--- vsode/pattern.c
+++ vsode/pattern.c
@@ -143,6 +143,41 @@
 //     }
 // }
 
+// *     *
+//  *   * 
+//   * *  
+//    *   
+//   * *  
+//  *   * 
+// *     *
+// #include <stdio.h>
+// int main() {
+//   int a;
+//   scanf("%d", &a);
+
+//   for (int i = 1; i <= a; i++) {
+
+//     for (int j = 1; j <= a * 2-1; j++) {
+//      if (i==j||i + j == a * 2)
+//         printf("*");
+//     else
+//         printf(" ");
+//      }
+//     printf("\n");
+//   }
+  
+//   for (int i = a-1; i >0; i--) {
+
+//     for (int j = 1; j <= a * 2-1; j++) {
+//      if (i==j||i + j == a * 2)
+//         printf("*");
+//     else
+//         printf(" ");
+//      }
+//     printf("\n");
+//   }
+// }
+
 // #include<stdio.h>
 
 // int main (){
