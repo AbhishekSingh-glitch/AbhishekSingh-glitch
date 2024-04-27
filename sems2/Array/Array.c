4d5a49873557061c4492888319049a2174c423d3
Abhishek Singh <abhishek120106@gmail.com>
2024-04-27 22:34:43 +0530

    Update Array.c

--- sems2/Array/Array.c
+++ sems2/Array/Array.c
@@ -1,24 +1,24 @@
 // collection of similar type of data type
-/*
-#include<stdio.h>
-int main(){
-int a[5]={5,6},i=0;
-int b[5]={};
-// printf("%d %d %d %d %d",a[0], a[1] ,a[2] ,a[3], a[4]);
-while(i<5){ printf("%d ",a[i]); i++; } 
-printf("\n");i=0;
-while(i<5){ printf("%d ",b[i]); i++; } 
-}
-*/
 
-/*
-#include<stdio.h>
-int main(){
-    int a[10];
-    for(int i=0;i<10;i++) scanf("%d",&a[i]);
-    for(int i=0;i<10;i++) printf("%d ",a[i]);
-}
-*/
+// #include<stdio.h>
+// int main(){
+// int a[5]={5,6},i=0;
+// int b[5]={};
+// // printf("%d %d %d %d %d",a[0], a[1] ,a[2] ,a[3], a[4]);
+// while(i<5){ printf("%d ",a[i]); i++; } 
+// printf("\n");i=0;
+// while(i<5){ printf("%d ",b[i]); i++; } 
+// }
+
+
+
+// #include<stdio.h>
+// int main(){
+//   int a[10];
+//   for(int i=0;i<10;i++) scanf("%d",&a[i]);
+//   for(int i=0;i<10;i++) printf("%d ",a[i]);
+// }
+
 
 // #include<stdio.h>
 // int main()
@@ -48,19 +48,8 @@ int main(){
 //    printf("%d ",i);
 // }
 
-// #include<stdio.h>
-// int main(){
-//     // array size should be <= 10^6 and some times it depend on 
-//     int a[5],sum=0;
-//     for(int i=1;i<=5;i++)
-//         scanf("%d",&a[i]);
 
-//    for(int i=1;i<=5;i++)
-//         sum+=a[i];
-//    printf("%d",sum);
-// }
-
-
+// sum of all elements
 // #include<stdio.h>
 // int main(){
    
@@ -73,6 +62,7 @@ int main(){
 //    printf("%d",a[0]);
 // }
 
+// searching
 // #include<stdio.h>
 // int main(){
 //     int a[5],x,f=0;
@@ -80,7 +70,7 @@ int main(){
 //     for(int i=1;i<=5;i++)
 //         scanf("%d",&a[i]);
 
-//     printf("Enter element");
+//     printf("Enter element ");
 //     scanf("%d",&x);
 
 //     for(int i=1;i<=5;i++)
@@ -91,42 +81,24 @@ int main(){
 
 // }
 
-// #include<stdio.h>
-// int main(){
-//     int a[5],b,cnt=0;
-    
-//     for(int i=0;i<5;i++)
-//         scanf("%d",&a[i]);
-
-//     scanf("%d",&b);
-    
-//     for(int i=0;i<5;i++)
-//         if(a[i]>b)
-//             cnt++;
-    
-//     printf("%d",cnt);
-// }
 
 
+// 2nd greatest in array
 // #include<stdio.h>
 // #include<limits.h>
 // int main(){
-//     int a[10],m=0,u=0,l=INT_MIN,k=INT_MIN;
-
+//     int a[10],m=0,l=INT_MIN,k=INT_MIN;
 //     for(int i=0;i<5;i++)
 //         scanf("%d",&a[i]);
-
 //     A:
 //     for(int i=0;i<5;i++){
 //         if(l<a[i])
 //             l=a[i];
-//         u++;
-
+//             m++;
 //         if(k<a[i]&&a[i]!=l)
 //             k=a[i];
 //     }
-//     if(u==5&&m==0){goto A; m++;}
-
+//     if(m==5){ m++; goto A;}
 //     printf("%d ",k);
 // }
 
@@ -139,30 +111,16 @@ int main(){
 //     fun(a);
 // }
 
-
+// tells which two elements sum is = to input no.
 // #include<stdio.h>
 // int main(){
-
-// int b,a[]={1,2,3,4,5,6};
-
-// scanf("%d",&b);
-// int n=sizeof(a)/sizeof(int);
-// for(int j=0;j<n;j++)
-// for(int i=j+1;i<n;i++){
-//     if(b==a[j]+a[i]) printf("%d=%d+%d\n",b,a[j],a[i]);
-// }
-
-// }
-
-// duplicate elements
-// #include<stdio.h>
-// int main(){
-// int a[]={1,2,3,4,5,6,6};
-
-// int n=sizeof(a)/sizeof(int);
-// for(int j=0;j<n;j++)
-// for(int i=j+1;i<n;i++)
-//     if(a[j]==a[i]) printf("%d\n",a[j]);
+//   int b,a[]={1,2,3,4,5,6};
+//   scanf("%d",&b);
+//   int n=sizeof(a)/sizeof(int);
+//   for(int j=0;j<n;j++)
+//   for(int i=j+1;i<n;i++)
+//     if(b==a[j]+a[i]) 
+//       printf("%d=%d+%d\n",b,a[j],a[i]);
 // }
 
 
@@ -171,13 +129,10 @@ int main(){
 // int main(){
 
 // int e=0,o=0,a[]={1,2,3,4,5,6};
-
 // int n=sizeof(a)/sizeof(int);
-
 // for(int j=0;j<n;j++)
 //     if(j%2==0)  e+=a[j];  
 //     else o+=a[j];
-
 // printf("%d",e-=o);
 // }
 
@@ -186,31 +141,22 @@ int main(){
 
 // #include <stdio.h>
 // void p(int arr[],int n){
-//   int i = 0;
-//   int j = n - 1;
+//   int i = 0, j = n - 1;
 //   while (i <= j) {
 //     int temp = arr[i];
 //     arr[i] = arr[j];
 //     arr[j] = temp;
-//     ++i;
-//     --j;
+//     ++i; --j;
 //   }
 // }
-
 // int main() {
-
 //   int arr[] = {3, -1, 8, 5, 4, 9, 2};
 //   int n = sizeof(arr) / sizeof(int);
-//   for (int i = 0; i < n; ++i)  
-//     scanf("%d ", &arr[i]);
-//   for (int i = 0; i < n; ++i) // actual array
-//     printf("%d ", arr[i]);
-  
+//   for (int i = 0; i < n; ++i)  scanf("%d ", &arr[i]);
+//   for (int i = 0; i < n; ++i)  printf("%d ", arr[i]);
 //   printf("\n");
 //   p(arr,n);
-//   for (int i = 0; i < n; ++i) { // after reversing
-//     printf("%d ", arr[i]);
-//   }
+//   for (int i = 0; i < n; ++i)  printf("%d ", arr[i]);
 //   return 0;
 // }
 
@@ -222,33 +168,41 @@ int main(){
 //   int n = sizeof(a) / sizeof(int);
 //   printf("duplicate elements\n");
 //   for (int j = 0; j < n; j++){
- 
 //     for (int i = j + 1; i < n; i++)
-    
 //       if (a[j] == a[i] && b != a[j]) {
 //         printf("%d\n", a[i]);
 //         b = a[j];
 //       }
- 
 // }
 // }
 
-
-#include <stdio.h>
-int main() {
-  int b = 0, a[10],k ;
-  for(int i=0;i<5;i++) scanf("%d",&a[i]);
-  for(int i=0;i<5;i++) printf("%d ",a[i]);
-  int n = sizeof(a) / sizeof(int);
-  scanf("%d",&k);
-
-  for (int j = 0; j < n; j++){
- 
-    for (int i = j + 1; i < n; i++)
-    
-    
-      }
-  for(int i=0;i<5;i++) printf("%d ",a[i]);
-}
+// unique element
+// #include <stdio.h>
+// int main() {
+//   int b = 0,c=0, a[10];
+//   for(int i=0;i<10;i++) scanf("%d",&a[i]);
+//   int n =sizeof(a)/sizeof(int);
+//   printf("unique elements\n");
+//   for (int j = 0; j <n; j++)
+//     for (int i = j + 1; i <= n; i++)
+//       if (a[j] != a[i] && b!=a[i]&&c!=a[j]) {
+//         printf("%d\n", a[j]);
+//         b = a[i];
+//         c=a[j];
+//       }
+// }
 
 
+// checking array is sorted or not
+
+// #include<stdio.h>
+// int main(){
+//   int flag=0;
+//   int arr[]={1,2,3,6,5};
+//   int arr[]={1,2,3,4,5};  
+//   int n=sizeof(arr)/sizeof(int);
+//     for(int i=0;i<n-1;i++)
+//       if(arr[i]<arr[i+1]) flag++;
+//     if(flag==n-1) printf("sorted");
+//     else printf("unsorted");
+// }
