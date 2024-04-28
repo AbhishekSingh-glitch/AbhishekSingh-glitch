4f1cfc37d13c453776a062c158cbd9dd453c7311
Abhishek Singh <abhishek120106@gmail.com>
2024-04-29 00:09:35 +0530

    Update Array.c

--- sems2/Array/Array.c
+++ sems2/Array/Array.c
@@ -176,7 +176,7 @@
 // }
 // }
 
-// unique element
+// unique element      //wrong
 // #include <stdio.h>
 // int main() {
 //   int b = 0,c=0, a[10];
@@ -199,7 +199,7 @@
 // int main(){
 //   int flag=0;
 //   int arr[]={1,2,3,6,5};
-//   int arr[]={1,2,3,4,5};  
+//   //int arr[]={1,2,3,4,5};  
 //   int n=sizeof(arr)/sizeof(int);
 //     for(int i=0;i<n-1;i++)
 //       if(arr[i]<arr[i+1]) flag++;
