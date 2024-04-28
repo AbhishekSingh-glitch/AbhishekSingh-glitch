156baa4414177d9790d9f98c728aa3d050605f41
Abhishek Singh <abhishek120106@gmail.com>
2024-04-28 12:46:53 +0530

    Update string.c

--- sems2/string/string.c
+++ sems2/string/string.c
@@ -32,3 +32,71 @@ int main(){
     for(int i=0;i<10;i++) scanf("%c",&a[i]);
     for(int i=0;i<10;i++) printf("%c",a[i]);
 }
+
+
+// #include<stdio.h>
+// int main ()
+// {
+//     int a=0,i=0;
+//     char s[10];
+//     gets(s);
+//     while(s[i]!='\n'){
+//         if(s[i]=='a')
+//         ++a;
+//     ++i;
+//     }
+//     puts(s);
+//     printf("\n%d times 'a' is used",a);
+//     return 0;
+
+// }
+
+
+// #include<stdio.h>
+// int main ()
+// {
+//     int a=0,i=0;
+//     char s[100];
+//     gets(s);
+//     while(s[i]!='\n'){
+//         if(s[i]==' ')
+//         a++;
+//     ++i;
+//     }
+//     puts(s);
+//     printf("\n%d times 'space' is used",a);
+//     return 0;
+// }
+
+
+// #include<stdio.h>
+// int main ()
+// {
+//     int a=0,i=0;
+//     char s[10];
+//     gets(s);
+//     while(s[i]!='\n'){
+//         if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'
+//          ||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U')
+//         a++;
+//     ++i;
+//     }
+//     puts(s);
+//     printf("\n%d times 'vowel' is used",a);
+// return 0;
+// }
+
+
+// #include<stdio.h>
+// int main ()
+// {
+//     int a=0,i=0;
+//     char s[10];
+//     for(char i='\n';i!=s[a-1];){
+//         scanf("%c",&s[a]);++a;
+//     }
+    
+//     puts(s);
+//     return 0;
+// }
+
