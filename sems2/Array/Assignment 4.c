3419a16048c5c98e077303ecff32e02e0450c7ba
Abhishek Singh <abhishek120106@gmail.com>
2024-04-28 21:34:46 +0530

    Update Assignment 4.c

--- sems2/Array/Assignment 4.c
+++ sems2/Array/Assignment 4.c
@@ -1,51 +1,53 @@
 //WAP to Calculate the product of all the elements in the given array.
+#include<stdio.h>
+int main(){
+    int a[10];
+    int n=sizeof(a)/sizeof(int);
 
+    for(int i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    
+    for(int i=1;i<n;i+)
+        a[0]*=a[i];
+
+    printf("%d",a[0]);
+}
 //WAP to find the second largest element in the given Array in one pass. (one pass means using only 1 loop)
 
 //WAP to find the minimum value out of all elements in the array.
-
 //(Example: arr[5] = {2, 60, 11, 4, 42};
-                  //output-> 2
-//)
+//output-> 2
+
 
 //WAP to find the largest three elements in the array.
-
 //(Example: arr[5] = {2, 60, 11, 4, 42};
-                  //output-> 60 42 11
-//)
+//output-> 60 42 11
+
 
 //WAP to check if the given array is sorted(in ascending order)or not.
 
 //WAP to find the difference between the product of elements at even indices to the sum of elements at odd indices.
 
-//Given an array of integers, change the value of all odd indexed elements to its second multiple and
-//increment all even indexed values by 10.
+//Given an array of integers, change the value of all odd indexed elements to its second multiple and increment all even indexed values by 10.
 
-//If an array arr contains n elements, then check if the given array is a palindrome or not. 
-
+//If an array arr contains n elements, then check if the given array is a palindrome or not.
 //(Example: int arr[5] = {1, 2, 3, 2, 1};
-                   //output-> Palindrome
-
+//output-> Palindrome
 //int arr[5] = {1, 2, 3, 4, 2, 1};
 //output-> Not Palindrome
-//)
 
 //WAP to Merge two arrays nums1 and nums2 (both in descending order) into a single array sorted in decreasing order.
 
 //WAP to find the smallest missing positive element in the sorted Array. (Take array as user input)
-
 //(Example: //input-> 1, 2, 3, 5, 6, 7
-                   //output-> 4
-
+//output-> 4
 //input-> -5,-1,1, 2, 3, 4, 6
 //output-> 5
-//)
 
 
 //WAP to find the first non-repeating element in the array.
-
 //(Example: int arr[n] = {1, 1, 2, 56, 89, 56, 2, 31, 2, 89, 45, 34};
-                   //output-> 31
-//)
+//output-> 31
+
 
 //WAP to find and display all the common elements of two arrays 'nums1' and 'nums2'. Print -1 if there are no common elements.
