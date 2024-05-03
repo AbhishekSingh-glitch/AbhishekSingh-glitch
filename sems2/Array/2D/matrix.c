56480119c317f8de366c80a234a4f0525dbd5cfb
Abhishek Singh <abhishek120106@gmail.com>
2024-05-03 23:01:13 +0530

    Update matrix.c

--- sems2/Array/2D/matrix.c
+++ sems2/Array/2D/matrix.c
@@ -25,20 +25,23 @@ int main() {
     int a[3][3],ar[3][3];
     int temp;
     for(int i=0;i<3;i++)
-    for(int j=0;j<3;j++)
-    scanf("%d",&a[i][j]);
-     printf("\n");
+        for(int j=0;j<3;j++)
+            scanf("%d",&a[i][j]);
+
+    printf("\n");
      
     for(int i=0;i<3;i++)
-    for(int j=0;j<i;j++){
-        temp=a[i][j];
-        a[i][j]=a[j][i];
-        a[j][i]=temp;}
+        for(int j=0;j<i;j++){
+            temp=a[i][j];
+            a[i][j]=a[j][i];
+            a[j][i]=temp;
+        }
     
     for(int i=0;i<3;i++){
-    for(int j=0;j<3;j++)
-    printf("%d ",a[i][j]);
-    printf("\n");}
+        for(int j=0;j<3;j++)
+            printf("%d ",a[i][j]);
+        printf("\n");
+    }
     
     
    
