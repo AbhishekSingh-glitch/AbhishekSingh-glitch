2b92686ac521845be2b80e5684b1ab92ae4d5764
Abhishek Singh <abhishek120106@gmail.com>
2024-05-06 23:08:32 +0530

    Update true.c

--- true.c
+++ true.c
@@ -79,15 +79,62 @@
 //   return 0;
 // }
 
-#include<stdio.h>
-#include<stdlib.h>
-int main(){
-  char *sen= "hello world";
+// #include <stdio.h>
+// #include<string.h>
+// #include <stdlib.h>
+// #include <time.h>
+// #include <unistd.h>
 
-  while(1){
-    printf("%s\n",sen);
-    system("sleep 1");
-    system("clear");
-    }
-  
+// int main() {
+
+//   while (1) {
+
+//     time_t t;
+//     struct tm *tm;
+
+//     t = time(0);
+//     tm = localtime(&t);
+//     sleep(0);
+    
+//     int hour = tm->tm_hour;
+//     char period[3]="AM";
+
+//     if (hour >= 12) {
+//         if (hour > 12)
+//             hour -= 12;
+//         strcpy(period, "PM");
+    
+//     }
+
+//     printf("%d:%02d:%02d %s\n", hour, tm->tm_min, tm->tm_sec, period);
+//     sleep(1);
+//     system("cls");
+//   }
+
+//   return 0;
+// }
+
+#include <stdio.h>
+#include<string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+void customSleep(int milliseconds) {
+    struct timespec ts;
+    ts.tv_sec = milliseconds / 1000; // Convert milliseconds to seconds
+    ts.tv_nsec = (milliseconds % 1000) * 1000000; // Convert remaining milliseconds to nanoseconds
+
+    // Sleep for the specified time
+    nanosleep(&ts, NULL);
+}
+
+int main() {
+char a[]="Selecting colors not simply for play, but to infuse the world with their presence, speaks to a profound understanding of the human psyche and the intricate interplay between perception and emotion. It signifies a conscious endeavor to enrich our environments with the nuanced tapestry of feelings and associations that colors evoke, transcending mere aesthetics to touch upon the very essence of our shared human experience. It is an expression of the universal longing to imbue our surroundings with depth, meaning, and soulful resonance, inviting contemplation, connection, and transformation in the vibrant hues that adorn our lives.";
+  int n=sizeof(a)-1;
+  for(int i=0;i<n;i++) {
+
+    customSleep(80);
+    printf("%c",a[i]);
+  }
+  return 0;
 }
