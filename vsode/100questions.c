e141b66d6571e244964d82037ab086eff6261bc4
Abhishek Singh <abhishek120106@gmail.com>
2024-05-07 17:40:44 +0530

    Update 100questions.c

--- vsode/100questions.c
+++ vsode/100questions.c
@@ -1,17 +1,23 @@
-#include<stdio.h>
-#include<math.h>
+
+
 #include<time.h>
 
 #define pi 3.14
 
-int add();     // function
+    // function
+
+
 int main()
 // 1 C Program to print text
+#include<stdio.h>
+int main()
 {
     printf("text");
     return 0;
 }
 // 2 C Program To Read Two Numbers And Print The Sum Of Given Two Numbers.
+#include<stdio.h>
+int main()
 {
     int a,b;
     printf("Enter 2 numbers to add\n")
@@ -21,6 +27,8 @@ int main()
     return 0;
 } 
 // 3 C Program To Accept Student Roll No, Marks in 3 Subjects and Calculate Total, Average and Printin
+#include<stdio.h>
+int main()
 {
     int a,b,c,d;
 
@@ -38,7 +46,8 @@ int main()
     printf("\nAverage : %d",A);
 }
 // 4 C Program To Read Three Numbers And Print The Biggest Of Given Three Numbers 
-{
+#include<stdio.h>
+int main(){
     int a,b,c;
     printf("Enter a no. ");
     scanf("%d",&a);
@@ -46,30 +55,19 @@ int main()
     scanf("%d",&b);
     printf("Enter a no. ");
     scanf("%d",&c);
-
-    if(a>b){
-        if(a>c){
-            printf("%d",a);}
-        else{
-            printf("%d",c);
-        }
-
-    }
-    else if(a<b){
-        if(b>c){
-            printf("%d",b);}
-        else{
-            printf("%d",c);
-        }
-
-    }
-    else{
-            printf("%d",c);
-    }
-
+    a=a>b&&a>c?a:b>a&&b>c?b:c;
+    printf("%d",a);
+//     if(a>b)
+//         if(a>c) printf("%d",a);
+//         else printf("%d",c);
+//     else if(a<b)
+//         if(b>c) printf("%d",b);
+//         else printf("%d",c);
+//     else   printf("%d",c);
 }
 // 5 C Program To Read & Number And Find Whether The Given Number is Even Or Odd
-{
+#include<stdio.h>
+int main(){
     int a;
     printf("Enter a no.");
     scanf("%d",&a);
@@ -79,26 +77,19 @@ int main()
     printf("odd");
 }
 // 6 C Program to accept a year and check whether the given year is leap year or not
-{
+#include<stdio.h>
+int main(){
     int a;
     printf("enter a year to check leap year or not\n");
     scanf("%d",&a);
 
-    if(a%4==0){
-        if(a%100==0){
-            if(a%400==0){
-                printf("leap year");
-            }
-            else{
-                printf("not a leap year");
-            }
-        }
-        else{printf("leap year");
-        }
-    }
-    else{
-        printf("not a leap year");
-    }
+    if(a%4==0)
+        if(a%100==0)
+            if(a%400==0)    printf("leap year");
+            else    printf("not a leap year");
+        else printf("leap year");
+    else    printf("not a leap year");
+    
     return 0;
 }
 // 7 C Program to print Individual Digits
@@ -106,7 +97,9 @@ int main()
 // 8 C Program to accept a three digit number and print the sum of individual digits.
 
 // 9 C Program to accept a number and check the given number is Armstrong or not.
-{
+#include<stdio.h>
+#include<math.h>
+int main(){
     int n,a,b,sum=0;
     scanf("%d",&n);
     a=n;
@@ -115,16 +108,14 @@ int main()
         sum+=pow(b,3);
         n/=10;
     }
-    if (a==sum) {
-        printf("Armstrong number\n");
-    }
-    else {
-        printf("not an Armstrong number");
-    }
+    if (a==sum)    printf("Armstrong number\n");
+    else     printf("not an Armstrong number");
+    
     return 0;
 }
 // 10 C Program to print ODD numbers from 1 to 10
-{
+#include<stdio.h>
+int main(){
     int a;
     // while(a==0)
     while(a<=10){
@@ -135,7 +126,8 @@ int main()
     return 0;
 }
 // 11 C Program to print natural numbers from 1 to 20 in Reverse
-{
+#include<stdio.h>
+int main(){
     int a=20;
     while(a!=0){
         printf("%d\n",a);
@@ -144,16 +136,18 @@ int main()
     return 0;
 }
 // 12 C Program to print sum of the natural numbers from 1 tο 10.
-{
+#include<stdio.h>
+int main(){
     int a=10,b;
-    while(a>=1){
+    while(a>0){
     b+=a;
     --a;   
     }
     printf("%d",b);
 }
 // 13 C Program to accept a number and print mathematical table of the given no.
-{
+#include<stdio.h>
+int main(){
     int a,b,c=1;
     printf("enter the no. of which you want table ");
     scanf("%d",&a);
@@ -169,19 +163,19 @@ int main()
     return 0;
 }
 // 14 C Program to print 1 to 10 mathematical tables.
-{
+#include<stdio.h>
+int main(){
     int n;
     int i;
     printf("Which table you want\n");
     scanf("%d",&n);
     for(i=n;i<=n*10;i=i+n)  //increase the zeroes you want
-    {
         printf("%d\n",i);
-    }
     return 0;
 }
 // 15 C Program to print fibonacci series.
-{
+#include<stdio.h>
+int main(){
     int n, a = 0, b = 1, c;
 
     printf("Enter the number of terms: ");
@@ -203,14 +197,14 @@ int main()
     return 0;
 }
 // 16 C Program to print numeric pyramid
-{
+#include<stdio.h>
+int main(){
     int a=5,b=1;                       
     for(int i=0;i<a;i++){
         for(int j=a;j>i;j--) 
         printf(" ");              
-        for(int j=0;j<=i;j++){    
-            printf("%d ",b);
-        }     
+        for(int j=0;j<=i;j++)  
+            printf("%d ",b);    
         b++;
         printf("\n");
     }
@@ -218,14 +212,14 @@ int main()
 // 17 C Program to print numerical pyramid II.
     
 // 18 C Program to print numerical diamond.
-{
+#include<stdio.h>
+int main(){
     int a=4,b=1; 
       for(int i=0;i<=a;i++){
         for(int j=a;j>i;j--)      
         printf(" ");              
-        for(int j=0;j<=i;j++){    
-            printf("%d ",b);         
-        }    
+        for(int j=0;j<=i;j++)  
+            printf("%d ",b);           
         b++;                    
         printf("\n");      
     }             
@@ -233,37 +227,38 @@ int main()
     for(int i=0;i<a;i++){         
         for(int j=0;j<=i;j++)     
         printf(" ");              
-        for(int j=a;j>i;j--){    
-            printf("%d ",b);       
-        } 
+        for(int j=a;j>i;j--)   
+            printf("%d ",b);        
         b--;                        
         printf("\n");
     }                  
 }
 // 19 C Program to print character pyramid.
-{
+#include<stdio.h>
+int main(){
     int a=5; 
     char b='A';
     for(int i=0;i<a;i++){
         for(int j=a;j>i;j--)     
         printf(" ");              
-        for(int j=0;j<=i;j++){    
+        for(int j=0;j<=i;j++)   
             printf("%c ",b);         
-        }         
+                 
         b++;//  * * * * *
         printf("\n");
     }
 }
 // 20 C Program to print character diamond.
-{
+#include<stdio.h>
+int main(){
     int a=4;
     char b='A'; 
       for(int i=0;i<=a;i++){
         for(int j=a;j>i;j--)    
         printf(" ");              
-        for(int j=0;j<=i;j++){    
+        for(int j=0;j<=i;j++)   
             printf("%c ",b);         
-        }    
+            
         b++;                     
         printf("\n");             
     }             
@@ -271,15 +266,16 @@ int main()
     for(int i=0;i<a;i++){      
         for(int j=0;j<=i;j++)     
         printf(" ");              
-        for(int j=a;j>i;j--){    
+        for(int j=a;j>i;j--)   
             printf("%c ",b);       
-        } 
+        
         b--;                        
         printf("\n");
     }                  
 }
 // 21 C Program to find biggest of two no by using ternary numbers 
-{
+#include<stdio.h>
+int main(){
     int a,b;
     printf("enter no. to check greater");
     scanf("%d%d",&a,&b);
@@ -293,7 +289,8 @@ int main()
 // 24 C Program to accept a year and check the given year is leap or not by using ternary 
 
 // 25 C Program to accept a character in the uppercase and print in lower case
-{
+#include<stdio.h>
+int main(){
     char a;
     
     scanf("%c",&a);
@@ -306,7 +303,8 @@ int main()
     return 0;
 }
 // 26 C Program to accept a character in any case and print in another case.
-{
+#include<stdio.h>
+int main(){
     char a;
     
     scanf("%c",&a);
@@ -323,7 +321,8 @@ int main()
     return 0;
 }
 // 27 C Program to natural number from 1 to 10 by using while loop
-{
+#include<stdio.h>
+int main(){
     int a=1;
     while(a<=10){ 
     printf("%d\n",a);
@@ -341,7 +340,8 @@ int main()
 // 32 C Program to accept a string and count no of capital letters, no. of small fetters and no, of special characters
     
 // 33 C Program to accept any single digit number and print it in words.
-{
+#include<stdio.h>
+int main(){
     int a;
     printf("enter a single digit\n");
     scanf("%d",&a);
@@ -394,7 +394,8 @@ int main()
     return 0;
 }
 // 34 C Program to print prime numbers between 1 to 100
-{
+#include<stdio.h>
+int main(){
   int a;
 
   for (int i=2; i<=100; i++) {
@@ -410,6 +411,9 @@ int main()
   return 0;
 }
 // 35 C Program to accept two numbers and print sum of two numbers by using functions 
+#include<stdio.h>
+int add(); 
+int main()
 {
     printf("enter two no. to add\n");
     add();
@@ -423,7 +427,8 @@ add(){
     return 0;
 }
 // 36 C Program to accept a number and find factorial of given number
-{
+#include<stdio.h>
+int main(){
     int a,b=1;
     printf("enter no. to find factorial");
     scanf("%d",&a);
@@ -437,7 +442,8 @@ add(){
 // 37 C Program to accept a number and check the given number Armstrong or not (Processill
     
 // 38 C Program to accept a number and print the sum of given and Reverse number
-{
+#include<stdio.h>
+int main(){
     int a,n,r,b;
     scanf("%d", &a);
     while (a != 0) {
@@ -477,7 +483,8 @@ add(){
 // 51 C Program to print "PASCAL TRIANGLE" Process!!. 
     
 // 52 C Program to check whether a given number is perfect or not.
-{
+#include<stdio.h>
+int main(){
    int a,n=1,b=0,z=0;
    scanf("%d",&a);
    
@@ -496,7 +503,8 @@ add(){
    return 0;
 }  
 // 53 C Program to check whether a given number is prime number.
-{
+#include<stdio.h>
+int main(){
    int a,n=1,b=0;
    scanf("%d",&a);
    
@@ -518,8 +526,6 @@ add(){
 // 56 C Program to accept two number and print the sum of given two number by using pointers
     
 // 57 C Programs to multiply two Matrices
-    
-// 58 C Program to print prime number between 1-100
 
 // 59 C Program to accept a string and find the length of the string
     
@@ -532,15 +538,39 @@ add(){
 // 63 C Program to find area of a triangle when there sides are given. 
     
 // 64 C Program to print Armstrong number between 1-500.
-
-// 65 C Program to check whether a given number is Armstrong or not
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int n,a=1,c,b,sum=0;
+    while(a<=500){
+        n=a;
+        while(n>0) {
+            b=n%10;
+            sum+=pow(b,3);
+            n/=10;
+        }
+        if (a==sum)    printf("%d ",a);
+        a++;
+    }
+    return 0;
+}
     
 // 66 C Program to print the floyd's' triangle
     
 // 67 C Program to read data in 3 structures and print
     
 // 68 C Program to print a diagonal matex.
-    
+#include<stdio.h>
+int main(){
+    int s;
+    scanf("%d",&s);
+    for(int i=0;i<s;i++){
+        for(int j=0;j<s;j++)
+        if(i==j) printf("*");
+        else printf(" ");
+        printf("\n");
+    }
+}
 // 69 C Program to copy contents of one file into another
 
 // 70 C Program to create a file of number and copy odd number into second file and even number into third file
@@ -552,7 +582,8 @@ add(){
 // 73 C Program to sort the entered elements using selection sort technique.
     
 // 74 C Program to find number whether a is divisible by '11' or not without actual division. 
-{ 
+#include<stdio.h>
+int main(){ 
     int a,b,c;
     printf("enter a number to check 11 divisible\n");
     scanf("%d",&a);
@@ -577,7 +608,8 @@ add(){
     return 0;
 }
 // 75 C Program to find maximum and minimum of entered 'n' number using arrays.
-{
+#include<stdio.h>
+int main(){
     int a[],b,l=a[1];
     printf("Enter how many numbers to compare");
     scanf("%d",&b);
@@ -593,13 +625,13 @@ add(){
     }
     printf("%d",l);
 }
-
 // 76 C Program to print the following series until the sum exceeds 2.6 term value exceeds 1.5 x+x2/21+x3/31+.......
     
 // 77 C Program to print a frequency distribution table for a dass ul 20-students in the foliowing format. The marks range form 1-25.
     
 // 78 C Program to accept values into an array and print array in reverse and original format by using three different functions.
-{
+#include<stdio.h>
+int main(){
     int a[]={1,2,3,4,5},i=0;
     while(i<5){
         scanf("%d",&a[i]);
@@ -624,7 +656,7 @@ add(){
 // 84 C Program to accept two numbers and print the sum of given two numbers by using pointers reverse of the given string by using functions
     
 // 85 C Program to accept a string and print 
-    
+
 // 86 C Program to accept two 3 dimensional array and store subtraction of these two arrays into third array
     
 // 87 C Program to accept a single dimensional array and print them by using pointers
@@ -634,13 +666,14 @@ add(){
 // 89 C Program to print 4 dimentional motro with constant number
     
 // 90 C Prongram to accept a string and print each word in reverse
-    
+
 // 91 C Program to accept elements into single dimensional array and print the array in ascending order by using three different arrays.
     
 // 92 C Program to accept data and store the given date into file print the date:
     
 // 93 C Program to accept data in lower case and store the given data into file into upper case and print the data.
-{
+#include<stdio.h>
+int main(){
     char a;
     
     scanf("%c",&a);
@@ -664,9 +697,32 @@ add(){
 // 96 C Program to accept a string in lower case 
     
 // 97 C Program to accept two numbers and interchange two Values using functions
-    
+#include<stdio.h>
+void swap(int *a,int *b);
+int main(){
+    int a,b;
+    scanf("%d%d",&a,&b);
+    swap(&a,&b);
+    printf("%d %d",a,b);
+}
+void swap(int *a,int *b){
+    int t=*a;
+    *a=*b;
+    *b=t;
+}
 // 98 C Program for example of static variable.
     
 // 99 C Program to accept a string and print by trailing spaces
     
 // 100 C Program to print anti diagonal.
+#include<stdio.h>
+int main(){
+    int s;
+    scanf("%d",&s);
+    for(int i=0;i<s;i++){
+        for(int j=0;j<s;j++)
+        if(i+j==s-1) printf("*");
+        else printf(" ");
+        printf("\n");
+    }
+}
