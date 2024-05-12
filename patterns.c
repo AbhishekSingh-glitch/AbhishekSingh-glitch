9d435c966ae6cf2c975c0b72bfab63ff327a6b2f
Abhishek Singh <abhishek120106@gmail.com>
2024-05-12 12:19:18 +0530

    Update patterns.c

--- patterns.c
+++ patterns.c
@@ -995,25 +995,18 @@
 // #include<stdio.h>
 
 // int main (){
-//     int a=4;                
-//     for(int i=0;i<a;i++){
-//         for(int j=0;j<i;j++)      
-//         printf(" ");              
-//         for(int j=a;j>=i;j--){    
-//             printf("* ");         
-//         }                         
-//         printf("\n");             
-//     }                             
-//     for(int i=0;i<=a;i++){        
-//         for(int j=a;j>i;j--)      
-//         printf(" ");              
-//         for(int j=0;j<=i;j++){    
-//             printf("* ");         
-//         }                         
-//         printf("\n");
-//     }
-// }
+//     int a;
+//     scanf("%d", &a);
 
+//     for (int i = 1; i <2* a; i++) {
+//         for (int j = 1; j<a*2; j++) 
+//             if (i==j||i==1||i==2*a-1||i+j==a*2)
+//                 printf("*");
+//             else
+//                 printf(" ");
+//     printf("\n");
+//   }
+// }
 
 //  *   * 
 //   * *  
@@ -1026,25 +1019,12 @@
 //   int a;
 //   scanf("%d", &a);
 
-//   for (int i = 1; i <= a; i++) {
-
-//     for (int j = 1; j <= a * 2-1; j++) {
-//      if (i==j||i + j == a * 2)
-//         printf("*");
-//     else
-//         printf(" ");
-//      }
-//     printf("\n");
-//   }
-  
-//   for (int i = a-1; i >0; i--) {
-
-//     for (int j = 1; j <= a * 2-1; j++) {
-//      if (i==j||i + j == a * 2)
-//         printf("*");
-//     else
-//         printf(" ");
-//      }
+//     for (int i = 1; i <2* a; i++) {
+//         for (int j = 1; j<a*2; j++) 
+//             if (i==j||i+j==a*2)
+//                 printf("*");
+//             else
+//                 printf(" ");
 //     printf("\n");
 //   }
 // }
