326bf761ec68fb79ba505cf7e55d39d1f202badf
Abhishek Singh <abhishek120106@gmail.com>
2024-05-13 18:46:02 +0530

    Create as5.c

---
+++ as5.c
@@ -0,0 +1,62 @@
+// 1 WAP to store 10 at every index of a 2D matrix with 5 rows and 5 columns.
+
+// 2 WAP to add two matrices and save the result in one of the given matrices. 
+// (Do not use extra array)
+
+// Given a matrix ‘A’ of dimension n x m and 2 coordinates (l1, r1) and (l2, r2). Return the sum of the
+// rectangle from (l1,r1) to (l2, r2).
+// Below is a given matrix which sample input & output:-
+// Sample input 1: l1 = 1, r1 = 2 , l2 = 3 , r2 = 3
+// Output 1: -4
+// Sample input 2: 2: l1 = 1, r1 = 0 , l2 = 0 , r2 = 3
+// Output 2:  2   
+
+// 4 Write a program to find the largest element of a given 2D array of integers.
+
+// 5 WAP to print the row number having the maximum sum in a given matrix.
+// Example:
+// input: 
+// 1 3 5 7
+// 3 4 7 8 
+// 1 4 12 3 
+// output:   2
+// Explanation : The 2nd row has the maximum sum i.e.1+4+12+3 = 20
+
+// 6 Write a program to print the elements of both the diagonals in a square matrix.
+// Example:
+// Input:
+// 1 2 3 
+// 4 5 6
+// 7 8 9
+// Output:
+// 1   3
+//   5
+// 7   9
+
+// 7 Write a program to rotate the matrix by 90 degrees anti-clockwise.
+
+// 8 Given an m x n integer matrix, if an element is 0, set its entire row and column to 0's. 
+// Suppose there exists only one zero in the entire matrix. Do not use extra array.
+// Example:
+// Input : matrix = [
+//     [1,1,1],
+//     [1,0,1],
+//     [1,1,1]]
+// Output : [
+//     [1,0,1],
+//     [0,0,0],
+//     [1,0,1]]
+
+// 9 WAP to displays the elements of middle row and the elements of middle column.
+// [Assuming the 2D Array to be a square matrix with odd dimensions i.e. 3x3, 5x5, 7x7 etc...]
+
+// 10 Given a matrix, WAP to arrange column elements in ascending order using C program.
+// Example:
+// Matrix: 
+//  9 8 7 
+//  5 4 6 
+//  1 2 3 
+// Matrix after sorting column elements: 
+//  1 2 3 
+//  5 4 6 
+//  9 8 7
