622c21db378ab1e8a14fff90c15bc3396f108208
Abhishek Singh <abhishek120106@gmail.com>
2024-05-16 09:51:05 +0530

    Update sorting.c

--- sems2/Array/sorting.c
+++ sems2/Array/sorting.c
@@ -148,4 +148,20 @@
 // next least element 
 
 
-// 
+// bubble short
+#include<stdio.h>
+int main(){
+    
+    int a[]={3,4,1,5,2};
+    int n=sizeof(a)/sizeof(int);
+    for(int j=0;j<n-1;j++)
+    for(int i=0;i<n-1-j;i++)
+    if(a[i]>a[i+1]){
+        int temp=a[i];
+        a[i]=a[i+1];
+        a[i+1]=temp;
+    }
+    for(int i=0;i<n;i++)
+    printf("%d ",a[i]);
+       
+}
