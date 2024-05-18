4a13201ef08d1b05cb12a0ebfaefd95f53ff811f
Abhishek Singh <abhishek120106@gmail.com>
2024-05-18 21:33:36 +0530

    Create leap year.c

---
+++ vsode/leap year.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("enter a year to check leap year or not\n");
+    scanf("%d",&a);
+
+    if(a%4==0)
+        if(a%100==0)
+            if(a%400==0) printf("leap year");
+            else printf("not a leap year");
+        else printf("leap year");
+    else printf("not a leap year");
+    
+    return 0;
+}
