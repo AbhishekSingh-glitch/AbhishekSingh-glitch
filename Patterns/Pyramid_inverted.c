319c39edbb0812efa9bd46734dae083f905c12b8
Abhishek Singh <abhishek120106@gmail.com>
2024-05-21 15:13:04 +0530

    Update Pyramid_inverted.c

--- Patterns/Pyramid_inverted.c
+++ Patterns/Pyramid_inverted.c
@@ -2,12 +2,12 @@
 int main (){
     int a;    
     scanf("%d",&a);                   
-    for(int i=a-1;i>=0;i--){              
-        for(int j=0;j<a+i;j++)
-            if(a-1>i+j)
-                printf(" ");
+    for(int i=0;i<a;i++){              
+        for(int j=0;j<a+a-1;j++)
+            if(i<=j&&i+j<=a*2-2)
+                printf("*");
             else
-                printf("*");         
+                printf(" ");         
                          
         printf("\n");
     }
