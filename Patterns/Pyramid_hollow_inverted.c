d6b290e56ea9068a01a0eaacd3212548f8cfca58
Abhishek Singh <abhishek120106@gmail.com>
2024-05-21 15:13:03 +0530

    Update Pyramid_hollow_inverted.c

--- Patterns/Pyramid_hollow_inverted.c
+++ Patterns/Pyramid_hollow_inverted.c
@@ -2,9 +2,9 @@
 int main (){
     int a;    
     scanf("%d",&a);                   
-    for(int i=a-1;i>=0;i--){              
-        for(int j=0;j<a+i;j++)
-            if(a-1==i+j||a-1==j-i||i==a-1)
+    for(int i=0;i<a;i++){              
+        for(int j=0;j<a+a-1;j++)
+            if(i==j||a+a-2==j+i||i==0)
                 printf("*");
             else
                 printf(" ");         
