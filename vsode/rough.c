2a2dbb6a2744d6366ea4f9811d7ccf69c86b2217
Abhishek Singh <abhishek120106@gmail.com>
2024-05-22 12:41:46 +0530

    Update rough.c

--- vsode/rough.c
+++ vsode/rough.c
@@ -86,43 +86,100 @@
 // }
 
 
+// #include <stdio.h>
+
+// int main() 
+// {
+    
+//   int  a[2] = {1,3},b[1] = {2};
+//   int n=sizeof(a)/sizeof(int);
+//   int m=sizeof(b)/sizeof(int);
+//   int c[n+m],k=0,i=0,j=0;
+//   float q,w;
+//     while(i<n&&j<m){
+//         if(a[i]<b[j]) {
+//             c[k]=a[i];
+//             i++;}
+//         else{
+//             c[k]=b[j];
+//             j++;}
+//         ++k;
+//     }
+
+//     while(i<n){
+//         c[k]=a[i];
+//         i++;
+//         k++;
+//     }
+//     while(j<m){
+//         c[k]=b[j];
+//         j++;
+//         ++k;
+//     }
+//     i=0,j=n+m-1;
+//     while(i<=j){
+//         q=c[i];
+//         w=c[j];
+//         i++;
+//         j--;
+//     }
+//     q=(q+w)/2;
+//     printf("%f",q);
+// }
+
+#include <stdio.h>
+int isValid(char *s,int n) {
+    int c,h=0;
+    for(int i=0;i<n;i++){
+        c=0;
+        for(int j=0;j<n;j++)
+            if( s[i]=='('&&s[j]==')'||s[i]=='{'&&s[j]=='}'||s[i]=='['&&s[j]==']'&&i!=j
+            ||s[j]=='('&&s[i]==')'||s[j]=='{'&&s[i]=='}'||s[j]=='['&&s[i]==']'){
+                printf("%c",s[i]);
+                c++;
+            }
+        if(c==0) break;
+    }
+    if(c==0) 
+    return 0;
+    else
+    return 1;
+}
+int main() {
+    char t[]= "{[]}";
+    int n=sizeof(t)-1;
+    for(int i=0;i<n;i++)
+    printf("%c",t[i]);
+printf("\n");
+    int g=isValid(t,n);
+    printf("\n%d",g);
+    return 0;
+}
+
+
+char* triangleType(int* s) {
+    
+    if (s[0] + s[1] > s[2] && s[1] + s[2] > s[0] && s[0] + s[2] > s[1]) {
+    // printf("it can be a");
+    if (s[0] == s[1] && s[0] == s[2] && s[2] == s[1])
+       return 'e';
+    else if (s[0] == s[1] || s[1] == s[2] || s[0] == s[2])
+       return 'i';
+    else
+       return 's';
+  }
+  else {
+     return 0;
+  }
+  
+}
 #include <stdio.h>
 
-int main() 
-{
-    
-  int  a[2] = {1,3},b[1] = {2};
-  int n=sizeof(a)/sizeof(int);
-  int m=sizeof(b)/sizeof(int);
-  int c[n+m],k=0,i=0,j=0;
-  float q,w;
-    while(i<n&&j<m){
-        if(a[i]<b[j]) {
-            c[k]=a[i];
-            i++;}
-        else{
-            c[k]=b[j];
-            j++;}
-        ++k;
-    }
-
-    while(i<n){
-        c[k]=a[i];
-        i++;
-        k++;
-    }
-    while(j<m){
-        c[k]=b[j];
-        j++;
-        ++k;
-    }
-    i=0,j=n+m-1;
-    while(i<=j){
-        q=c[i];
-        w=c[j];
-        i++;
-        j--;
-    }
-    q=(q+w)/2;
-    printf("%f",q);
+int main() {
+  int s[3];
+  printf("Enter 3 no. ");
+  scanf("%d%d%d", &s[0], &s[1], &s[2]);
+  printf("%c",triangleType( s));
+  
+  
 }
