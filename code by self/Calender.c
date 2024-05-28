5855eea4a8b4a8b6d04f6712ee599971b48aa889
Abhishek Singh <abhishek120106@gmail.com>
2024-05-28 23:32:54 +0530

    Create Calender.c

---
+++ code by self/Calender.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    int f=0,md[]={31,29,31,30,31,30,31,31,30,31,30,31};
+    char mn[][10]={"January", "February", "March","April", "May", "June",
+                 "July", "August", "September", "October","November","December"};
+    char w[]="SMTWtFs";
+
+    for(int m=0;m<12;m++){
+        printf("%s\n",mn[m]);
+
+        for(int i=0;i<7;i++)
+            printf("%c\t",w[i]);
+
+        printf("\n");
+
+        for(int d=1;d<=md[m];d++){
+            printf("%d\t",d,md[d]);
+
+            if(d%7==0)
+                printf("\n");
+        }
+
+        printf("\n");
+    }
+}
