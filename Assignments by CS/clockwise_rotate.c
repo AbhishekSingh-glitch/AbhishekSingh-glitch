ca9d92fd4359e97471086a59083accc2fff16446
Abhishek Singh <abhishek120106@gmail.com>
2024-06-04 13:19:16 +0530

    Update clockwise_rotate.c

--- Assignments by CS/clockwise_rotate.c
+++ Assignments by CS/clockwise_rotate.c
@@ -1,30 +1,32 @@
 #include<stdio.h>
 int main(){
-	int a[3][3]={1,2,3,4,5,6,7,8,9};
-	
-	for(int i=0;i<3;i++){
-	  	for(int j=0;j<3;j++)
-	    	printf("%d ",a[i][j]);
-		printf("\n");
-	}
+    int s=4;
+	printf("enter the size of the matrix\n");
+    scanf("%d",&s); 
+	int a[s][s];
+
+	for(int i=0;i<s;i++)
+        for(int j=0;j<s;j++)
+            scanf("%d",&a[i][j]);
+
 	// transpose of matrix
-	for(int i=0;i<3;i++)
-	  for(int j=0;j<i;j++){
+	for(int i=0;i<s;i++)
+	  	for(int j=0;j<i;j++){
 	        int temp=a[i][j];
         	a[i][j]=a[j][i];
         	a[j][i]=temp;
 		}
     // arranging 
-   	for(int i=0;i<3;i++)
-	  for(int j=0;j<1;j++){
-	        int temp=a[i][2];
-        	a[i][2]=a[i][j];
+   	for(int i=0;i<s;i++)
+	  	for(int j=0,l=s-1;j<s/2;j++,l--){
+	        int temp=a[i][l];
+        	a[i][l]=a[i][j];
         	a[i][j]=temp;
 		}
 
     printf("\n");
-    for(int i=0;i<3;i++){
-		for(int j=0;j<3;j++)
+    for(int i=0;i<s;i++){
+		for(int j=0;j<s;j++)
 			printf("%d ",a[i][j]);
 		printf("\n");
 	}
