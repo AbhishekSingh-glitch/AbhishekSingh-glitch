c07fde22555b36a950a2b7171f249ec3eccef665
Abhishek Singh <abhishek120106@gmail.com>
2024-09-26 19:44:59 +0530

    Update Power.c

--- vsode/Power.c
+++ vsode/Power.c
@@ -45,3 +45,17 @@ int main(){
     printf("%d",d);
     return 0;
 }
+
+
+double myPow(double a,double b) {
+
+    double d = 1.0,t;
+    t=b>0?1:-1;
+    for (int c = 0; c !=b;) {
+        c+=t;
+        d *= a;
+        }
+    return t>0?d:1/d;
+}
+
+    
