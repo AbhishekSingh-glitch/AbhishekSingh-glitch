fcc4d30d3fde82b4daf01e0874aa3dc3557e320b
Abhishek Singh <abhishek120106@gmail.com>
2024-09-28 17:43:16 +0530

    Update tic_tac_toe.c

--- code by self/tic_tac_toe.c
+++ code by self/tic_tac_toe.c
@@ -3,16 +3,16 @@
 
 void assign(char* bin,int x,int y,int *t);
 void printbin(char *bin,int x,int y,int *t);
-int prir(char *bin,char s,char s2);
-int pril(char *bin,char s,char s2);
-int prih(char *bin,char s,char s2);
-int priv(char *bin,char s,char s2);
-int check(char* bin);
+int prir(char *bin,char sign);
+int pril(char *bin,char sign);
+int prih(char *bin,char sign,int pos);
+int priv(char *bin,char sign,int pos);
+int check(char* bin,char sign,int pos);
 int che(int n,int *t);
 
 int main(){
     char bin[9]={'\0'};
-    int r=0;
+    int r=0,ele;
     int q=0,x=-1,t[9]={-1,-1,-1,-1,-1,-1,-1,-1,-1},y=-2;
     system("cls");
     printbin(bin,x,y,t);
@@ -20,148 +20,125 @@ int main(){
         while(1){
             printf("\nenter no for X ");
             scanf("%d",&x);
+            ele=x-1;
             system("cls");
             int go=che(x,t);
             printbin(bin,x,y,t);
             if(go&&(x>0&&x<=9)){ x=0; break;}
         }
-        r=check(bin);
+        r=check(bin,'X',ele);
         q++;
         if(r==1||q>=8) break;
         
         while(1){
             printf("\nenter no for O ");
             scanf("%d",&y);
+            ele=x-1;
             system("cls");
             int go=che(y,t);
             printbin(bin,x,y,t);
             if(go&&(y>0&&y<=9)){ y=0; break;}
         }
-        r= check(bin);
+        r= check(bin,'O',ele);
         if(r==1) break;
         q++;
         
     }
-    if(q==9) printf("Draw\n");
+    if(r!=1) printf("\t\nDraw\n");
 }
-int check(char* bin){
+int check(char* bin,char sign,int pos){
     int a=0;
-    // for right side diagonal
-    a= (prir(bin,'X','O'));
-    if(a==1) return 1;
+    
+    a = prih(bin, sign, pos);
+    if (a==1){
+        printf("\n\t%c wins", sign);
+        return 1;
+    }
 
-    // //for left side diagonal
-    a=(pril(bin,'X','O'));
-    if(a==1) return 1;
+    a = priv(bin, sign, pos);
+    if (a==1){
+        printf("\n\t%c wins", sign);
+        return 1;
+    }
 
-    // for horizontal // 1st line
-    a= (prih(bin,'X','O')); 
-    if(a==1) return 1;
+    switch (pos){
+    // left top
+        case 0:
+            a = pril(bin, sign);
+            break;
 
-    // 2nd line
-    a=(prih(bin,'X','O'));
-    if(a==1) return 1;
+        // right top
+        case 2:
+            a = prir(bin, sign);
+            break;
 
-    // 3rd line
-    a=(prih(bin,'X','O'));
-    if(a==1) return 1;
-
-    // for vertical // 1st line
-    a= (priv(bin,'X','O'));
-    if(a==1) return 1;
-
-    // 2nd line
-    a= (priv(bin,'X','O'));
-    if(a==1) return 1;
-
-    // 3rd line
-    a=(priv(bin,'X','O'));
-    if(a==1) return 1;
-}
-
-int prir(char *bin,char s,char s2){
-    int rtd=0,rtd2=0;
-    for (int i = 2; i < 7; i += 2){
-            if(bin[i]=='X')  rtd++;
-            if(rtd==3){
-                printf("\n\tX wins");
+        // center
+        case 4:
+            a = pril(bin, sign);
+            if (a==1){
+                printf("\n\t%c wins", sign);
                 return 1;
             }
-            if(bin[i]=='O') rtd2++;
-            if(rtd2==3){
-                printf("\n\tO wins");
-                return 1;
-            }
-        }    rtd = 0; rtd2=0;
-}
-int pril(char *bin,char s,char s2){
-    int lfd=0,lfd2 = 0;
-    for (int i = 0; i < 9; i += 4){
-            if(bin[i]==s)  lfd++;
-            if(lfd==3){
-                printf("\n\tX wins");
-                return 1;
-            }   
-            if(bin[i]==s2)  lfd2++;
-            if(lfd2==3){
-                printf("\n\tO wins");
-                return 1;
-            }             
-        }  lfd = 0; lfd2 = 0;
-}
-int prih(char *bin,char s,char s2){
-    int hrz=0,hrz2=0,j=0,j2=0;
-    for(int i=0;i<8;i+=3)
-        if(bin[i]=='X'){
-            j=i;
+            a = prir(bin, sign);
             break;
-        }
-    for (int i = j; i < j+3; i++){
-        if (bin[i] == s)    hrz++;
-        if (hrz == 3){
-            printf("\n\tX wins");
-            return 1;
-        }
+
+        // left bottom
+        case 6:
+            a = prir(bin, sign);
+            break;
+
+        // right bottom
+        case 8:
+            a = pril(bin, sign);
+            break;
     }
-    for(int i=0;i<8;i+=3)
-        if(bin[i]=='O'){
-            j2=i;
-            break;
-        }
-    for (int i = j2; i < j2+3; i++){
-        if (bin[i] == s2)    hrz2++;
-        if (hrz2 == 3){
-            printf("\n\tO wins");
-            return 1;
-        }
-    }   hrz =0;  hrz2 = 0;
-}
-int priv(char *bin,char s,char s2){
-    int vrt=0,vrt2=0,j=0,j2=0;
-    for(int i=0;i<3;i++)
-        if(bin[i]==s){
-            j=i;
-            break;
-        }
-    for (int i = j; i < j+7; i += 3) {
-        if (bin[i] == s) vrt++;
-        if (vrt== 3) {
-            printf("\n\tX wins");
-            return 1;
-        }
+    if (a==1){
+        printf("\n\t%c wins", sign);
+        return 1;
     }
-    for(int i=0;i<3;i++)
-        if(bin[i]==s2){
-            j2=i;
-            break;
-        }
-    for (int i = j2; i < j2+7; i += 3) {
-        if (bin[i] == s2) vrt2++;
-        if (vrt2== 3) {
-            printf("\n\tO wins");
-            return 1;
-        }
-    } vrt = 0; vrt2 = 0;
+}
+
+int prir(char *bin,char sign){
+    int cnt=0;
+    for (int i = 2; i < 7; i+=2)
+        if(bin[i]==sign) 
+            cnt++;
+
+    if (cnt == 3) return 1;
+    else return 0;
+}
+int pril(char *bin,char sign){
+    int cnt=0;
+    for (int i = 0; i < 9; i+=4)
+        if(bin[i]==sign) 
+            cnt++;
+
+    if (cnt == 3) return 1;
+    else return 0;
+}
+int prih(char *bin,char sign,int pos){
+    int cnt=0;
+    if(pos<=2) pos= 0;
+    else if(pos<=5) pos=3;
+    else if(pos<=8) pos =6;
+
+    for (int i = pos; i < pos+3; i++)
+        if (bin[i] == sign)
+            cnt++;
+
+    if (cnt == 3) return 1;
+    else return 0;
+}
+int priv(char *bin,char sign,int pos){
+    int cnt=0;
+    pos = pos % 3;
+
+    for (int i = pos; i < pos+7; i += 3) 
+        if (bin[i] == sign) 
+            cnt++;
+
+    if (cnt == 3) return 1;
+    else return 0;
 }
 
 void printbin(char *bin,int x,int y,int *t){
