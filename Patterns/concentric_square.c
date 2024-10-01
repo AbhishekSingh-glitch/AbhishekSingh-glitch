f0b1c45e6dce3c1e258b68dac0f3ff35ce5ef32d
Abhishek Singh <abhishek120106@gmail.com>
2024-10-01 17:27:52 +0530

    Update concentric_square.c

--- Patterns/concentric_square.c
+++ Patterns/concentric_square.c
@@ -12,18 +12,16 @@ int main()
     int n;
     scanf("%d", &n);
   	
-      for(int i=1;i<2*n;i++){
-          for(int j=1;j<2*n;j++){
-              int a=i;
-              int b=j;
-              if(a>n) a= 2*n-i;
-              if(b>n) b= 2*n-j;
-              int num=min(a,b);
-              printf("%d ",n-num+1);
-          }
-          printf("\n");
-      }
-      
-      
+        for(int i=1;i<2*n;i++){
+            for(int j=1;j<2*n;j++){
+                int a=i;
+                int b=j;
+                if(a>n) a= 2*n-i;
+                if(b>n) b= 2*n-j;
+                int num=min(a,b);
+                printf("%d ",n-num+1);
+            }
+            printf("\n");
+        }
     return 0;
 }
